68915114dc6b5ebc0e86ddcf31e4380e6e505c60
iamtsb <retro@tsb.space>
2020-07-30 15:40:14 +0200

    changes in file header info

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php
 
 	/*
-	 * index.php
-	 * 
-	 * PCB component locator code is written & released by M.F. Wieland (TSB)
-	 * This project has initially been created for John "Chucky" Hertell
-	 * 
+	 * Component Locator Chucky (PCB component locator)
+	 *
+	 * Written & released by M.F. Wieland (TSB)
+	 *
 	 * Version 1.3
 	 * Release date: 30-7-2020
-	 * 
-	 * This work is licensed under the terms of the MIT license.  
-	 * For a copy, see <https://opensource.org/licenses/MIT>.
 	 *
-	 * See LICENSE file for more license information
+	 * This project has initially been created for John "Chucky" Hertell
+	 *  
+     * Licensed under the MIT License. See LICENSE file in the project root for full license information. 
+     *
+	 * This file is part of the Component Locator Chucky
 	 */
 
 	
